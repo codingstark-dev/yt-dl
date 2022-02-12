6a273a56a1744aab5ff72b5ea8aad01ee1496639
Himanshu Maurya <40685364+refoxdev@users.noreply.github.com>
2022-02-12 21:44:31 +0530

    fixed small bug

--- pages/api/download.ts
+++ pages/api/download.ts
@@ -2,7 +2,7 @@ import readline from 'readline';
 // External modules
 import ytdl from "ytdl-core";
 import { NextApiRequest, NextApiResponse } from 'next';
-// import ytmux from 'ytdl-core-muxer';
+import ytmux from 'ytdl-core-muxer';
 // Global constants
 const ref = 'https://www.youtube.com/watch?v=LPM1sY0ipNM';
 import ffmpegPath from 'ffmpeg-static';
@@ -38,36 +38,36 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
 
 // default export: the ffmpeg muxer
-const ytmux = (link, options = {}) => {
-    const result = new stream.PassThrough({ highWaterMark: options.highWaterMark || 1024 * 512 });
-    ytdl.getInfo(link, options).then(info => {
-        let audioStream = ytdl.downloadFromInfo(info, { ...options, quality: 'highestaudio' });
-        let videoStream = ytdl.downloadFromInfo(info, { ...options, quality: 'highestvideo' });
-        // create the ffmpeg process for muxing
-        let ffmpegProcess = cp.spawn(ffmpegPath, [
-            // supress non-crucial messages
-            '-loglevel', '8', '-hide_banner',
-            // input audio and video by pipe
-            '-i', 'pipe:3', '-i', 'pipe:4',
-            // map audio and video correspondingly
-            '-map', '0:a', '-map', '1:v',
-            // no need to change the codec
-            '-c', 'copy',
-            // output mp4 and pipe
-            '-f', 'matroska', 'pipe:5'
-        ], {
-            // no popup window for Windows users
-            windowsHide: true,
-            stdio: [
-                // silence stdin/out, forward stderr,
-                'inherit', 'inherit', 'inherit',
-                // and pipe audio, video, output
-                'pipe', 'pipe', 'pipe'
-            ]
-        });
-        audioStream.pipe(ffmpegProcess.stdio[3]);
-        videoStream.pipe(ffmpegProcess.stdio[4]);
-        ffmpegProcess.stdio[5].pipe(result);
-    });
-    return result;
-};
+// const ytmux = (link, options = {}) => {
+//     const result = new stream.PassThrough({ highWaterMark: (options as any).highWaterMark || 1024 * 512 });
+//     ytdl.getInfo(link, options).then(info => {
+//         let audioStream = ytdl.downloadFromInfo(info, { ...options, quality: 'highestaudio' });
+//         let videoStream = ytdl.downloadFromInfo(info, { ...options, quality: 'highestvideo' });
+//         // create the ffmpeg process for muxing
+//         let ffmpegProcess = cp.spawn(ffmpegPath, [
+//             // supress non-crucial messages
+//             '-loglevel', '8', '-hide_banner',
+//             // input audio and video by pipe
+//             '-i', 'pipe:3', '-i', 'pipe:4',
+//             // map audio and video correspondingly
+//             '-map', '0:a', '-map', '1:v',
+//             // no need to change the codec
+//             '-c', 'copy',
+//             // output mp4 and pipe
+//             '-f', 'matroska', 'pipe:5'
+//         ], {
+//             // no popup window for Windows users
+//             windowsHide: true,
+//             stdio: [
+//                 // silence stdin/out, forward stderr,
+//                 'inherit', 'inherit', 'inherit',
+//                 // and pipe audio, video, output
+//                 'pipe', 'pipe', 'pipe'
+//             ]
+//         });
+//         audioStream.pipe(ffmpegProcess.stdio[3]);
+//         videoStream.pipe(ffmpegProcess.stdio[4]);
+//         ffmpegProcess.stdio[5].pipe(result);
+//     });
+//     return result;
+// };
